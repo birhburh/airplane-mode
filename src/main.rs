d016f9c6cf0bf950b4cff616579307821a6f7af6
Birh Burh <birhburh@gmail.com>
2023-12-11 19:45:51 +0100

    Process only player square

--- src/main.rs
+++ src/main.rs
@@ -49,60 +49,96 @@ async fn main() {
         // clear_background(BLACK);
 
         let camera_height = tile_side * 11. * screen_height() / screen_width();
+        let camera_start = player.position.y - camera_height / 2. - tile_side;
+        let camera_end = player.position.y + camera_height / 2. + tile_side;
         let camera = Camera2D {
             target: vec2(tile_side * 11. / 2., player.position.y),
             zoom: vec2(1. / (tile_side * 11. / 2.), 1. / (camera_height / 2.)),
             ..Default::default()
         };
+
+        // let camera = Camera2D {
+        //     target: vec2(tile_side * 11. / 2., player.position.y + tile_side * 2.),
+        //     zoom: vec2(1. / (tile_side * 11. / 1.5), 1. / (camera_height / 1.5)),
+        //     ..Default::default()
+        // };
+
+        // let camera_width = tile_side * 78. * screen_width() / screen_height();
+        // let camera =
+        //     Camera2D::from_display_rect(Rect::new(0., tile_side * 78., camera_width, -tile_side * 78.));
+
         set_camera(&camera);
 
+        let colors = [RED, ORANGE, YELLOW, GREEN, SKYBLUE, BLUE, VIOLET];
+        let mut cur_color = 0;
         // draw map
+        let mut ends = Vec::new();
         for layer in &tiled_map.raw_tiled_map.layers {
             let offsetx = layer.offsetx.unwrap_or_default() as f32;
             let offsety = layer.offsety.unwrap_or_default() as f32;
-            let mut new_y = 0;
+            let mut start_y = 0;
+            let mut end_y = layer.height as f32 - 1.;
+            let mut start_set = false;
             for (_, y, tile) in tiled_map.tiles(&layer.name, None) {
-                if tile.is_some() {
-                    let real_y = y as f32 * tile_side + offsety;
+                let real_y = y as f32 * tile_side + offsety;
 
-                    if real_y > (player.position.y - camera_height / 2. - tile_side) {
-                        new_y = y;
-                        break;
+                if start_set && (real_y >= camera_end) {
+                    end_y = (y - 1) as f32;
+                    break;
+                }
+                if tile.is_some() {
+                    if !start_set && (real_y >= camera_start) {
+                        start_y = y;
+                        start_set = true;
                     }
                 }
             }
 
-            let new_y_f = new_y as f32 * tile_side + offsety;
-            let src = Rect::new(0., new_y as f32, 11., camera_height / tile_side + 2.);
-            let new_y = new_y_f;
-            if new_y < (player.position.y + camera_height / 2. + tile_side) {
+            let start_y_f = start_y as f32 * tile_side + offsety;
+            let layer_height = (end_y - start_y as f32) + 1.;
+            ends.push((start_y, end_y));
+            let src = Rect::new(0., start_y as f32, 11., layer_height);
+            if start_y_f > camera_start && start_y_f < camera_end {
                 tiled_map.draw_tiles(
                     &layer.name,
                     Rect::new(
                         offsetx,
-                        new_y,
+                        start_y_f,
                         tile_side * 11.,
-                        camera_height + tile_side * 2.,
+                        tile_side * layer_height,
                     ),
                     src,
                 );
-            }
-            for (x, y, tile) in tiled_map.tiles(&layer.name, None) {
-                if y > new_y as u32 + 10 {
-                    break;
-                }
-                if let Some(tile) = tile {
-                    if tile.id != 0 {
-                        draw_rectangle_lines(
-                            x as f32 * tile_side + offsetx,
-                            y as f32 * tile_side + offsety,
-                            tile_side,
-                            tile_side,
-                            1.,
-                            YELLOW,
-                        );
+                for (x, y, tile) in tiled_map.tiles(&layer.name, src) {
+                    if y as f32 > start_y as f32 + camera_height / tile_side + 1. {
+                        break;
+                    }
+                    let y = y as f32 * tile_side + offsety;
+                    if y < (player.position.y + camera_height / 1. + tile_side) {
+                        if let Some(tile) = tile {
+                            if tile.id != 0 {
+                                draw_rectangle_lines(
+                                    x as f32 * tile_side + offsetx,
+                                    y,
+                                    tile_side,
+                                    tile_side,
+                                    2.,
+                                    colors[cur_color],
+                                );
+                            }
+                        }
                     }
                 }
+
+                draw_rectangle_lines(
+                    2. * (cur_color + 1) as f32,
+                    start_y_f + 2. * (cur_color + 1) as f32,
+                    tile_side * 11. - 4. * (cur_color + 1) as f32,
+                    tile_side * layer_height - 4. * (cur_color + 1) as f32,
+                    2.,
+                    colors[cur_color],
+                );
+                cur_color += 1;
             }
         }
 
@@ -179,11 +215,20 @@ async fn main() {
             2.,
             BLUE,
         );
+        draw_rectangle_lines(
+            0.,
+            player.position.y - camera_height / 2.,
+            tile_side * 11.,
+            camera_height,
+            2.,
+            BLUE,
+        );
         set_default_camera();
 
         #[cfg(target_os = "android")]
         input_handler.draw();
-        // draw_text(&format!("{:?}", camera.target), 0.0, 64.0, 16., RED);
+        draw_text(&format!("{:?}", (camera_start, camera_end)), 0.0, 16.0, 16., RED);
+        draw_text(&format!("{:?}", ends), 0.0, 64.0, 16., RED);
         draw_circle(
             (screen_width() - 2.) / 2.,
             (screen_height() - 2.) / 2.,
